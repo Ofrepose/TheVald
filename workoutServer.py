bab4cbb77728aac838c2c9324ce5fc0158d282db
ofrepose <ofrepose@endquery.com>
2020-01-24 23:16:16 -0500

    Edited Application Code for publication

--- workoutServer.py
+++ workoutServer.py
@@ -48,8 +48,7 @@ app.config.update(
     MAIL_SERVER='smtp.gmail.com',
     MAIL_PORT=465,
     MAIL_USE_SSL=True,
-    MAIL_USERNAME = 'paynedanielfranklin@gmail.com',
-    MAIL_PASSWORD = 'Bright661ev173453'
+
     )
 
 
